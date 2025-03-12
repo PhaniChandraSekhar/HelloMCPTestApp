6a6eb255f35c2aa0fe32d9999a0edd8509fb8c40
Phani Chandra Sekhar Avagaddi <phaniavagaddi@gmail.com>
2025-03-12 07:59:22 +0530

    Add login page

---
+++ app/login/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+import { LoginForm } from '@/components/ui/login-form'
+
+export default function LoginPage() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-background">
+      <LoginForm />
+    </main>
+  )
+}
